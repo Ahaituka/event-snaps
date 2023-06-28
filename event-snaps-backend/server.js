ff019f6eb712a4ceed224fbc44fae4e913a336ae
Shubham <shubham.jain3567@gmail.com>
2023-06-27 18:53:15 -0700

    ready for deployment

--- event-snaps-backend/server.js
+++ event-snaps-backend/server.js
@@ -1,27 +1,19 @@
-const express = require('express');
-const multer = require('multer');
-const cors = require('cors');
-const { google } = require('googleapis');
-const key = require('./credentials.json');
-const AWS = require('aws-sdk');
+const express = require("express");
+const multer = require("multer");
+const cors = require("cors");
+const { google } = require("googleapis");
+const AWS = require("aws-sdk");
 const upload = multer();
-const { Readable } = require('stream');
-const { PrismaClient } = require('@prisma/client')
+const { Readable } = require("stream");
+const { PrismaClient } = require("@prisma/client");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 
 dotenv.config();
-
-const SCOPES = ['https://www.googleapis.com/auth/drive'];
-const FOLDER_ID = process.env.FOLDER_ID;
-
-const jwtClient = new google.auth.JWT(key.client_email, null, key.private_key, SCOPES, null);
-const drive = google.drive({ version: 'v3', auth: jwtClient });
-
 
 const rekognition = new AWS.Rekognition({
   accessKeyId: process.env.ACCESS_KEY_ID,
@@ -29,183 +21,212 @@ const rekognition = new AWS.Rekognition({
   region: process.env.AWS_REGION,
 });
 
+// const oauth2Client = new google.auth.OAuth2(
+//   process.env.GOOGLE_CLIENT_ID,
+//   process.env.GOOGLE_CLIENT_SECRET,
+//   process.env.GOOGLE_REDIRECT_URI
+// );
 
-const oauth2Client = new google.auth.OAuth2(
-  process.env.GOOGLE_CLIENT_ID,
-  process.env.GOOGLE_CLIENT_SECRET,
-  process.env.GOOGLE_REDIRECT_URI
-);
-
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 const asyncHandler = (fn) => (req, res, next) =>
   Promise.resolve(fn(req, res, next)).catch(next);
 
-app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).json({ message: 'An internal server error occurred' });
-});
+app.post(
+  "/api/compare",
+  upload.single("image"),
+  asyncHandler(async (req, res) => {
+    const uploadedImage = req.file.buffer;
+    const userId = req.body.userId;
+    const accountDetails = await getAccountDetails(userId);
+    const refreshToken = accountDetails.refresh_token;
 
-
-app.post('/api/compare', upload.single('image'), asyncHandler(async (req, res) => {
-  const uploadedImage = req.file.buffer;
-  const userId = req.body.userId;
-  const accountDetails = await getAccountDetails(userId);
-  const refreshToken = accountDetails.refresh_token;
-  oauth2Client.setCredentials({ refresh_token: refreshToken });
-  oauth2Client.refreshAccessToken();
-  const folderId = accountDetails.folderId;
-  const gdrive = google.drive({ version: 'v3', auth: oauth2Client });
-
-
-
-  // Fetch all images from Google Drive folder
-  try {
-    const driveResponse = await gdrive.files.list({
-      q: `'${folderId}' in parents`,
-    });
-
-    const imageFiles = driveResponse.data.files.filter((file) => file.mimeType.startsWith('image/'));
-
-    // Download images from Google Drive folder
-    const folderImages = await Promise.all(
-      imageFiles.map(async (file) => {
-        const imageData = await gdrive.files.get(
-          { fileId: file.id, alt: 'media' },
-          { responseType: 'arraybuffer' }
-        );
-
-        return {
-          id: file.id,
-          name: file.name,
-          buffer: Buffer.from(imageData.data),
-        };
-      })
+    const oauth2Client = new google.auth.OAuth2(
+      process.env.GOOGLE_CLIENT_ID,
+      process.env.GOOGLE_CLIENT_SECRET,
+      process.env.GOOGLE_REDIRECT_URI
     );
 
-    // Compare faces using AWS Rekognition
-    const matchingImages = [];
-    for (const folderImage of folderImages) {
-      try {
-        const rekognitionResponse = await rekognition
-          .compareFaces({
-            SourceImage: { Bytes: Buffer.from(uploadedImage, 'base64') },
-            TargetImage: { Bytes: Buffer.from(folderImage.buffer, 'base64') },
-            SimilarityThreshold: 70,
-          })
-          .promise();
+    oauth2Client.setCredentials({ refresh_token: refreshToken });
+    await oauth2Client.refreshAccessToken();
+    const folderId = accountDetails.folderId;
 
-        if (rekognitionResponse.FaceMatches.length > 0) {
-          matchingImages.push(folderImage);
+    const gdrive = google.drive({ version: "v3", auth: oauth2Client });
+
+    // Fetch all images from Google Drive folder
+    try {
+      const driveResponse = await gdrive.files.list({
+        q: `'${folderId}' in parents`,
+      });
+
+      const imageFiles = driveResponse.data.files.filter((file) =>
+        file.mimeType.startsWith("image/")
+      );
+
+      // Download images from Google Drive folder
+      const folderImages = await Promise.all(
+        imageFiles.map(async (file) => {
+          const imageData = await gdrive.files.get(
+            { fileId: file.id, alt: "media" },
+            { responseType: "arraybuffer" }
+          );
+
+          return {
+            id: file.id,
+            name: file.name,
+            buffer: Buffer.from(imageData.data),
+          };
+        })
+      );
+
+      // Compare faces using AWS Rekognition
+      const matchingImages = [];
+      for (const folderImage of folderImages) {
+        try {
+          const rekognitionResponse = await rekognition
+            .compareFaces({
+              SourceImage: { Bytes: Buffer.from(uploadedImage, "base64") },
+              TargetImage: { Bytes: Buffer.from(folderImage.buffer, "base64") },
+              SimilarityThreshold: 70,
+            })
+            .promise();
+
+          if (rekognitionResponse.FaceMatches.length > 0) {
+            matchingImages.push(folderImage);
+          }
+        } catch (error) {
+          console.error("Error comparing faces:", error.message);
         }
-      } catch (error) {
-        console.error('Error comparing faces:', error);
       }
+
+      // Generate signed URLs for matched images
+      const signedUrls = await Promise.all(
+        matchingImages.map(async (image) => {
+          const urlResponse = await gdrive.files.get({
+            fileId: image.id,
+            fields: "webContentLink",
+          });
+
+          return urlResponse.data.webContentLink;
+        })
+      );
+
+      res.status(200).json({
+        urls: signedUrls,
+        images: matchingImages,
+      });
+    } catch (error) {
+      throw new Error(
+        "Error fetching images from Google Drive folder:",
+        error.message
+      );
     }
-
-    // Generate signed URLs for matched images
-    const signedUrls = await Promise.all(
-      matchingImages.map(async (image) => {
-        const urlResponse = await drive.files.get({
-          fileId: image.id,
-          fields: 'webContentLink',
-        });
-
-        return urlResponse.data.webContentLink;
-      })
-    );
-
-    res.status(200).json({
-      urls: signedUrls,
-      images: matchingImages,
-    });
-  } catch (error) {
-    throw new Error('Error fetching images from Google Drive folder:');
-  }
-}));
+  })
+);
 
 const getAccountDetails = async (userId) => {
   const accountDetails = await prisma.account.findUniqueOrThrow({
     where: {
-      userId: userId
-    }
-  })
+      userId: userId,
+    },
+  });
   console.log(accountDetails);
   return accountDetails;
-}
+};
 
+app.post(
+  "/api/upload",
+  upload.array("images"),
+  asyncHandler(async (req, res) => {
+    const uploadedImages = req.files;
+    const userId = req.body.userId;
+    const accountDetails = await getAccountDetails(userId);
+    const refreshToken = accountDetails.refresh_token;
+    oauth2Client.setCredentials({ refresh_token: refreshToken });
+    oauth2Client.refreshAccessToken();
+    // oauth2Client.refreshAccessToken((err, tokens) => {
+    //   if (err) {s
+    //     console.error('Error refreshing access token:', err);
+    //   } else {
+    //     console.log('New access token:', tokens.access_token);
+    //   }
+    // });
+    const folderId = accountDetails.folderId;
+    const gdrive = google.drive({ version: "v3", auth: oauth2Client });
+    try {
+      const uploadedImageResponses = await Promise.all(
+        uploadedImages.map(async (image) => {
+          const driveUploadResponse = await gdrive.files.create({
+            requestBody: {
+              name: image.originalname,
+              mimeType: image.mimetype,
+              parents: [folderId],
+            },
+            media: {
+              mimeType: image.mimetype,
+              body: Readable.from(image.buffer), // Create a stream from the buffer
+            },
+          });
 
-app.post('/api/upload', upload.array('images'), asyncHandler(async (req, res) => {
-  const uploadedImages = req.files;
-  const userId = req.body.userId;
-  const accountDetails = await getAccountDetails(userId);
-  const refreshToken = accountDetails.refresh_token;
-  oauth2Client.setCredentials({ refresh_token: refreshToken });
-  oauth2Client.refreshAccessToken();
-  // oauth2Client.refreshAccessToken((err, tokens) => {
-  //   if (err) {s
-  //     console.error('Error refreshing access token:', err);
-  //   } else {
-  //     console.log('New access token:', tokens.access_token);
-  //   }
-  // });
-  const folderId = accountDetails.folderId;
-  const gdrive = google.drive({ version: 'v3', auth: oauth2Client });
-  try {
-    const uploadedImageResponses = await Promise.all(
-      uploadedImages.map(async (image) => {
-        const driveUploadResponse = await gdrive.files.create({
-          requestBody: {
-            name: image.originalname,
-            mimeType: image.mimetype,
-            parents: [folderId],
-          },
-          media: {
-            mimeType: image.mimetype,
-            body: Readable.from(image.buffer), // Create a stream from the buffer
-          },
-        });
+          return driveUploadResponse.data;
+        })
+      );
 
-        return driveUploadResponse.data;
-      })
-    );
+      res.status(200).json({ message: "Images uploaded successfully" });
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  })
+);
 
-    res.status(200).json({ message: 'Images uploaded successfully' });
-  } catch (error) {
-    throw new Error(error.message);
-  }
-}));
+app.get(
+  "/api/download/:fileId",
+  asyncHandler(async (req, res) => {
+    try {
+      const fileId = req.params.fileId;
 
+      // Get file metadata
+      const fileMetadata = await gdrive.files.get({
+        fileId: fileId,
+        fields: "mimeType,name",
+      });
 
-app.get('/api/download/:fileId', asyncHandler(async (req, res) => {
-  try {
-    const fileId = req.params.fileId;
+      // Download file from Google Drive
+      const file = await gdrive.files.get(
+        { fileId: fileId, alt: "media" },
+        { responseType: "arraybuffer" }
+      );
 
-    // Get file metadata
-    const fileMetadata = await drive.files.get({
-      fileId: fileId,
-      fields: 'mimeType,name',
-    });
+      // Set headers for the browser to download the file
+      res.setHeader(
+        "Content-Disposition",
+        `attachment; filename=${fileMetadata.data.name}`
+      );
+      res.setHeader("Content-Type", fileMetadata.data.mimeType);
 
-    // Download file from Google Drive
-    const file = await drive.files.get(
-      { fileId: fileId, alt: 'media' },
-      { responseType: 'arraybuffer' }
-    );
+      res.send(Buffer.from(file.data));
+    } catch (error) {
+      throw new Error(
+        "Error downloading file from Google Drive",
+        error.message
+      );
+    }
+  })
+);
 
-    // Set headers for the browser to download the file
-    res.setHeader('Content-Disposition', `attachment; filename=${fileMetadata.data.name}`);
-    res.setHeader('Content-Type', fileMetadata.data.mimeType);
+//display hello message on the home page
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-    res.send(Buffer.from(file.data));
-  } catch (error) {
-    throw new Error('Error downloading file from Google Drive');
-  }
-}));
-
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({ message: err.message });
+});
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
+// module.exports = app;
