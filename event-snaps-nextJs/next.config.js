f91e8cfc94ffcfe6e10fc9efd0c38cbf90ea070b
Shubham <shubham.jain3567@gmail.com>
2023-11-25 14:51:49 +0000

    temporary redirect

--- event-snaps-nextJs/next.config.js
+++ event-snaps-nextJs/next.config.js
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://photos.app.goo.gl/66XreqyWMPj4a9Km7',
+        permanent: true,
+      },
+    ]
+  },
+  experimental: {
+    serverActions: true,
+  },
+}
 
-module.exports = {
-    experimental: {
-      serverActions: true,
-    },
-  }
+module.exports = nextConfig;
