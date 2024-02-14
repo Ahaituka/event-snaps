38f71a0b4c45769cb190112c90bb8a4a36d4dd51
Shubham <shubham.jain3567@gmail.com>
2024-02-14 04:31:34 +0000

    revert back to main project

--- event-snaps-nextJs/next.config.js
+++ event-snaps-nextJs/next.config.js
@@ -1,17 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        destination: 'https://photos.app.goo.gl/mh4QU6h66MYyrUpf7',
-        permanent: true,
-      },
-    ]
-  },
-  experimental: {
-    serverActions: true,
-  },
-}
+const nextConfig = {}
 
-module.exports = nextConfig;
+module.exports = {
+    experimental: {
+      serverActions: true,
+    },
+  }
